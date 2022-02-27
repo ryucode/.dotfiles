cb946e454882be9611e0280d606fe993eb44ffe6
Coder <33174877+ryucode@users.noreply.github.com>
2022-02-27 08:10:30 -0500

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
 # I am using zsh instead of bash.  I was having some troubles using bash with
 # arrays.  Didn't want to investigate, so I just did zsh
+rm -rf zsh 
+sudo apt install zsh
+
 pushd $DOTFILES
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
 do
