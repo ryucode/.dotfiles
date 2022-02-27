596fd19c17120ae84481dd3dee5392ca1a3d0e31
Coder <33174877+ryucode@users.noreply.github.com>
2022-02-27 08:11:21 -0500

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -2,7 +2,8 @@
 # I am using zsh instead of bash.  I was having some troubles using bash with
 # arrays.  Didn't want to investigate, so I just did zsh
 rm -rf zsh 
-sudo apt install zsh
+sudo apt install stow -y
+sudo apt install zsh -y
 
 pushd $DOTFILES
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
